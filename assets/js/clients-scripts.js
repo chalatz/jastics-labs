5cecb38be515f3ff3d21f308a412e3cac328b39c
chalatz <chralatz@gmail.com>
2018-02-13 14:04:31 +0200

    adding arrow

--- assets/js/clients-scripts.js
+++ assets/js/clients-scripts.js
@@ -5,23 +5,36 @@ $(document).ready(function(){
         $("#our-clients__content").lightSlider({
             item: 4,
             controls: false,
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        item: 3
-                    }
-                },
-                {
-                    breakpoint: 460,
-                    settings: {
-                        item: 2
-                    }
-                }
-            ]
+            // responsive: [
+            //     {
+            //         breakpoint: 768,
+            //         settings: {
+            //             item: 3
+            //         }
+            //     },
+            //     {
+            //         breakpoint: 460,
+            //         settings: {
+            //             item: 2
+            //         }
+            //     }
+            // ]
         });
 
     };
+
+    var init_arrow = function(){
+
+        var first_client = $('.our-clients__item').first()
+            first_client_left = first_client.position().left,
+            first_client_width = first_client.width(),
+            arrow_pos = first_client_left + first_client_width * .25,
+            arrow_el = $('#our-clients__arrow');
+
+        arrow_el.css('left', arrow_pos + 'px');
+
+    };
+
 
     var clients_feedback = function(){
 
@@ -29,21 +42,34 @@ $(document).ready(function(){
 
         client.on('click', function(){
             
-            var client_id = $(this).attr('data-client');
+            var $this = $(this),
+                client_id = $this.attr('data-client');
 
             $('.our-clients__item').removeClass('our-clients__item--active');
             $('.our-clients__feedback-item').removeClass('our-clients__feedback-item--active');
 
-            $(this).addClass('our-clients__item--active');
+            $this.addClass('our-clients__item--active');
             $("#"+client_id).addClass('our-clients__feedback-item--active');
 
-            console.log('clicked ' + client_id);
+            var handle_arrow = function(el){
+                var el_left = el.position().left,
+                    el_width = el.width(),
+                    arrow_pos = el_left + el_width * .25;
+    
+                $('#our-clients__arrow').animate({
+                    left: arrow_pos + 'px'
+                },1000);
+            };
+
+            handle_arrow($this);
 
         });       
 
     };
     
     clients_slider();
+
+    init_arrow();
 
     clients_feedback();
 
